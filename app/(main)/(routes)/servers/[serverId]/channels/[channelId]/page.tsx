f62d5c6e4ed90c101bb22b2b8dc5c19080de09b5
Seth Heinzman <sethheinzman@Seths-MacBook-Pro.local>
2023-09-05 13:18:09 -0400

    final

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,12 +1,13 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client";
+
+import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
 import { ChatMessages } from "@/components/chat/chat-messages";
 import { MediaRoom } from "@/components/media-room";
-import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { redirectToSignIn } from "@clerk/nextjs";
-import { ChannelType } from "@prisma/client";
-import { redirect } from "next/navigation";
 
 interface ChannelIdPageProps {
   params: {
@@ -18,7 +19,9 @@ interface ChannelIdPageProps {
 const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   const profile = await currentProfile();
 
-  if (!profile) return redirectToSignIn();
+  if (!profile) {
+    return redirectToSignIn();
+  }
 
   const channel = await db.channel.findUnique({
     where: {
@@ -33,7 +36,9 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     },
   });
 
-  if (!channel || !member) redirect("/");
+  if (!channel || !member) {
+    redirect("/");
+  }
 
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
@@ -51,7 +56,10 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             type="channel"
             apiUrl="/api/messages"
             socketUrl="/api/socket/messages"
-            socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
+            socketQuery={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
             paramKey="channelId"
             paramValue={channel.id}
           />
@@ -59,16 +67,18 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             name={channel.name}
             type="channel"
             apiUrl="/api/socket/messages"
-            query={{ channelId: channel.id, serverId: channel.serverId }}
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
           />
         </>
       )}
-
       {channel.type === ChannelType.AUDIO && (
         <MediaRoom chatId={channel.id} video={false} audio={true} />
       )}
       {channel.type === ChannelType.VIDEO && (
-        <MediaRoom chatId={channel.id} video={true} audio={false} />
+        <MediaRoom chatId={channel.id} video={true} audio={true} />
       )}
     </div>
   );
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,16 +1,18 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
 
-import { NavigationAction } from "@/components/navigation/navigation-action";
-import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { NavigationItem } from "@/components/navigation/navigation-item";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Separator } from "@/components/ui/separator";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
+
   if (!profile) {
     return redirect("/");
   }
@@ -40,12 +42,15 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
-
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
         <ModeToggle />
         <UserButton
           afterSignOutUrl="/"
-          appearance={{ elements: { avatarBox: "h-[48px] w-[48px]" } }}
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
         />
       </div>
     </div>
